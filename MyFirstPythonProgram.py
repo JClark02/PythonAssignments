ef275271baeca3a09caf6f75413b5cbeb5560f50
JClark02 <jclark665@ivytech.edu>
2025-04-24 22:22:21 -0400

    Add files via upload

---
+++ MyFirstPythonProgram.py
@@ -0,0 +1,3 @@
+print("Hello World")
+print("I just wrote my first Python Program")
+print("This is so much fun")
---
+++ RainingCatsandDogs_JoshClark.py
@@ -0,0 +1,14 @@
+# This program will display the sentence:
+# "It is raining cats and dogs."
+
+# Set the variable for 'cats'
+c = "cats"
+
+# Set the variable for 'dogs'
+d = "dogs"
+
+# Create the sentence using string concatenation
+s = "It is raining " + c + " and " + d + "."
+
+# Print the final sentence to the screen
+print(s)
